5edd960d7572c3fa9e727601ee1cb3e8bd374304
clairecofelice <clairecofelice@claires-mbp.dhcp.wustl.edu>
2016-04-11 18:51:55 -0500

    Fixed Commit

---
+++ 03_paragraph/scripts.js
@@ -0,0 +1,4 @@
+document.write("<h1>Welcome to my page</h1>");
+document.write("<p>This is a paragraph. This is a paragraph. This is a paragraph.</p>");
+document.write("<p>This is another paragraph. This is another paragraph. This is another paragraph.</p>");
+document.write("<p>This is a third paragraph. This is a third paragraph. This is a third paragraph.</p>");
---
+++ 04_vars/scripts.js
@@ -0,0 +1,7 @@
+var content = "";
+
+content += "<h1>This is a heading</h1>";
+
+content += "<p>This is a paragraph.</p>";
+
+document.write(content);
